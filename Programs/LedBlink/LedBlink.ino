3b5e24c88d4a8d2a61a258f246db2a8e121dcae5
Amit Patange <amitpatange88@gmail.com>
2019-02-15 18:43:46 +0530

    exp32 commit first

---
+++ Programs/LedBlink/LedBlink.ino
@@ -0,0 +1,16 @@
+int ledPin = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello World !!");
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
